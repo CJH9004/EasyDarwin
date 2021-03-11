fc5dcc7cf2aa4cc29cf88aaa6f6b05f61fbb650b
CJH9004 <2573699004@qq.com>
2021-03-11 15:22:14 +0800

    edit readme

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/EasyDarwin/EasyDarwin/rtsp"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 	s := rtsp.GetServer()
 	fmt.Println(s.Start())
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# rtsp-server
+
+Forked from [EasyDarwin/EasyDarwin](https://github.com/EasyDarwin/EasyDarwin), keep rtsp server function and remove others.
